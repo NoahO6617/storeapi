81e38dc1f17bcafd0d678610622b468299020574
noahorellana5 <noahorellana5@gmail.com>
2026-01-20 16:50:42 +0000

    Para guardar user

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config'
 import * as express from "express";
 import productRoutes from "./src/endpoints/products";
+import userRoutes from "./src/endpoints/user";
 
 const app = express();
 const port = Number(process.env.PORT) || 8080;
@@ -8,6 +9,7 @@ const port = Number(process.env.PORT) || 8080;
 app.use(express.json());
 
 app.use('/products', productRoutes);
+app.use('/users', userRoutes); 
 
 app.get('/', (_req, res) => {
   res.send('API funcionando');
@@ -16,3 +18,5 @@ app.get('/', (_req, res) => {
 app.listen(port, () => {
   console.log(`Listening on http://localhost:${port}`);
 });
+
+
